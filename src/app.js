0b84fe5ac10ea90a64253970fda8fb93a74cb66c
viniciusviana73 <viniciusviana1975@gmail.com>
2025-06-22 03:56:54 -0300

    morgan logger

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 // --- 1. Importação de Todas as Rotas ---
-
 const usersRoutes = require('./routes/usersRoutes');
 const equipesRoutes = require('./routes/equipesRoutes');
 const jogadoresRoutes = require('./routes/jogadoresRoutes');
@@ -14,15 +14,19 @@ const resultadosRoutes = require('./routes/resultadosRoutes');
 
 const app = express();
 
+// Request logging
+app.use(morgan('combined'));
+
 // --- 2. Middlewares Globais ---
 app.use(cors({
     origin: '*',
     credentials: true
 }));
+
 app.use(bodyParser.json());
+
 // Linha para servir imagens estáticas (como logos de times da pasta 'public')
 app.use(express.static('public')); 
-
 
 // --- 3. Rotas da API ---
 app.use('/api/users', usersRoutes);
@@ -31,7 +35,6 @@ app.use('/api/jogadores', jogadoresRoutes);
 app.use('/api/partidas', partidasRoutes);
 app.use('/api/torneios', torneiosRoutes);
 app.use('/api/resultados', resultadosRoutes);
-
 
 // --- 4. Servir o Frontend 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
